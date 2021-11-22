038f770b956d911d2d0fac93f40317c2165d9c55
Rodrigo Torres <rtorres@intermediait.com>
2021-11-22 19:16:14 -0300

    Add change in HomeView

--- MovieApp/Home/View/HomeView.swift
+++ MovieApp/Home/View/HomeView.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class HomeView: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var moviesCollectionView: UITableView!
+    private var router = HomeRouter()
+    private var viewModel = HomeViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel.bind(view: self, router: router)
+        setupTableView()
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    
+    private func setupTableView() {
+        moviesCollectionView.delegate = self
+        moviesCollectionView.dataSource = self
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = moviesCollectionView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
+    }
+    
 }
--- MovieApp/Home/ViewModel/HomeViewModel.swift
+++ MovieApp/Home/ViewModel/HomeViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class HomeViewModel {
+    private weak var view: HomeView?
+    private var router: HomeRouter?
+    
+    func bind(view: HomeView, router: HomeRouter) {
+        self.view = view
+        self.router = router
+        self.router?.setSourceView(view)
+    }
+    
+}
